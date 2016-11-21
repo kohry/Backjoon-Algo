501253bc006a7ec686f837b1e7f2338ec5d98c61
kohry <gorakgarak@gorakgarak.com>
2016-11-21 23:00:23 +0900

    a

--- pre1121.java
+++ pre1121.java
@@ -14,22 +14,13 @@ import java.util.StringTokenizer;
 public class Solution {
 
 	static int TestCase;
-
 	static int N, M, K;
-
-	static double map[][];
-
 	static ArrayList<ArrayList<Point>> list = new ArrayList<>();
-
-	static double cache1[][];
-	static double cache2[][];
-	static int visited1[];
-	static int visited2[];
 	static double result;
 
 	public static void main(String[] args) throws Throwable {
 
-		System.setIn(new FileInputStream("1113.txt"));
+		System.setIn(new FileInputStream("1114.txt"));
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -42,15 +33,6 @@ public class Solution {
 			N = Integer.parseInt(st.nextToken());
 			M = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
-
-			// //implementation////
-
-			map = new double[N + 1][N + 1];
-			cache1 = new double[M + 2][N + 1];
-			cache2 = new double[M + 2][N + 1];
-
-			visited1 = new int[N + 1];
-			visited2 = new int[N + 1];
 
 			list.clear();
 			result = 0;
@@ -68,14 +50,10 @@ public class Solution {
 				int b = Integer.parseInt(st.nextToken());
 				double p = Double.parseDouble(st.nextToken());
 
-				map[a][b] = p;
-
 				list.get(a).add(new Point(a, b, p));
 
 			}
 
-			map[0][1] = 1;
-			map[0][N] = 1;
 			calc(1, 0, 1, 1, 0, N, 1);
 
 			System.out.printf("#%d %.3f \n", testCase, result);
